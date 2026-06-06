cf25be52ff2dab3da1ab6fd5615dc39c14a1b239
huurhuntergel <tergelwan64@gmail.com>
2026-06-06 19:13:50 +0900

    fix price formating and radio selectors

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,8 +1,10 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
 
 let fullHTML;
-cart.forEach(cartItem =>{
+cart.forEach((cartItem, index )=>{
   const productId = cartItem.productId;
   let matchingProduct;
   products.forEach((product)=>{
@@ -24,7 +26,7 @@ cart.forEach(cartItem =>{
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${(matchingProduct.priceCents/100).toFixed(2)}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -46,7 +48,7 @@ cart.forEach(cartItem =>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${index}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -59,7 +61,7 @@ cart.forEach(cartItem =>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${index}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -72,7 +74,7 @@ cart.forEach(cartItem =>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${index}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
---
+++ javascript-amazon-project-main/scripts/utils/money.js
@@ -0,0 +1,1 @@
+export function formatCurrency (priceCents) {return (priceCents/100).toFixed(2);}
